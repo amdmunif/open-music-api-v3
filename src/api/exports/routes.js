aa9f7d729205734ead110440e623b55dee46285c
amdmunif <ahmad.munif02@gmail.com>
2021-12-12 15:45:34 +0700

    penerapan export

---
+++ src/api/exports/routes.js
@@ -0,0 +1,10 @@
+const routes = (handler) => [{
+    method: 'POST',
+    path: '/export/songs',
+    handler: handler.postExportSongsHandler,
+    options: {
+        auth: 'songsapp_jwt',
+    },
+}, ];
+
+module.exports = routes;
